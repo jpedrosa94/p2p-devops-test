6ed23a1d523e07b281a84923b60fac82746fe610
Julio Pedrosa <you@nops.io>
2024-08-05 11:40:19 -0300

    test: auto update image

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w,"Hello, you've requested: %s\n", r.URL.Path)
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
     })
     fmt.Println("Web app running on localhost:3000")
     http.ListenAndServe(":3000", nil)
