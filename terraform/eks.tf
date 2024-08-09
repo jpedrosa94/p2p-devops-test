03b075e46cc2079f08b3edbc4b51330c2f3416d0
Júlio Pedrosa <141178807+jpedrosa94@users.noreply.github.com>
2024-08-09 09:57:32 -0300

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -57,33 +57,3 @@ module "eks" {
     }
   }
 }
-
-################################################################################
-# EKS Blueprints Addons
-################################################################################
-
-module "eks_blueprints_addons" {
-  source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.16"
-
-  cluster_name      = module.eks.cluster_name
-  cluster_endpoint  = module.eks.cluster_endpoint
-  cluster_version   = module.eks.cluster_version
-  oidc_provider_arn = module.eks.oidc_provider_arn
-  #
-  #   enable_aws_load_balancer_controller = true
-  #   enable_metrics_server               = true
-  #   enable_external_dns                 = true
-  #   external_dns = {
-  #     values = [templatefile("../helm/environments/${terraform.workspace}/cluster-bootstrap/values/external-dns.yaml", {})]
-  #   }
-  #   enable_argocd = true
-  #   argocd = {
-  #     name          = "argocd"
-  #     chart_version = "7.3.4"
-  #     repository    = "https://argoproj.github.io/argo-helm"
-  #     namespace     = "argocd"
-  #     values        = [templatefile("../helm/environments/${terraform.workspace}/argo-bootstrap/values/argo.yaml", {})]
-  #
-  #   }
-}
